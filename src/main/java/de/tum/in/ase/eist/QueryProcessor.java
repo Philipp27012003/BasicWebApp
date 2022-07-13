37ebc846fe6fd613211d0c64fc3b2bf9a2045455
philippschuler <phi3@web.de>
2022-07-13 15:43:40 +0200

    asd

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -36,6 +36,12 @@ public class QueryProcessor {
             int res = Integer.parseInt(a[3]) + Integer.parseInt(a[6]);
             return res + "";
         }
+        else if (query.contains("square")) {
+            //"d7067f50: which of the following numbers is both a square and a cube: 289, 16, 606, 94"
+            String [] a = query.split(" ");
+            String [] b = a[2].split(", ");
+            return Arrays.stream(b).mapToInt(k -> (int) Integer.parseInt(k)).filter(t -> t%2 == 0).filter(t -> t > 0).filter(t -> Math.sqrt(t) * Math.sqrt(t) == (float) t).filter(t -> Math.pow(t, (1.0/3.0)) * Math.pow(t, (1.0/3.0)) * Math.pow(t, (1.0/3.0)) == t).max().getAsInt() + "";
+        }
         else {
             return"";
         }
